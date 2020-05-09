f2e9962c81ba0d9639f764c9bf4c5bbf1859dd5a
Alexandr Moroz <afmoroz@gmail.com>
2020-05-10 01:52:51 +0300

    loop

--- timers.js
+++ timers.js
@@ -2,21 +2,23 @@ const fs = require("fs")
 
 console.log("start")
 
-Promise.resolve().then(() => console.log("promise")); 
-setImmediate(() => { console.log("immediate") }) 
-process.nextTick(() => console.log("tick"))
-
 fs.readFile(__filename, () => {
-  setTimeout(() => { console.log("1000ms") }, 1000) 
+  setTimeout(() => { console.log("1000ms") }, 1000)
   setTimeout(() => { console.log("0ms") }, 0)
   setTimeout(() => console.log("timeout"));
 
-  setImmediate(() => { console.log("immediate") })
+  setImmediate(() => { console.log("immediate 2") })
 
-  Promise.resolve().then(() => console.log("promise"));
+  Promise.resolve().then(() => console.log("promise 3"));
 })
 
-  
+Promise.resolve().then(() => console.log("promise 1"));
+setTimeout(() => console.log("timeout", 0));
+setImmediate(() => { console.log("immediate 1") })
+process.nextTick(() => console.log("tick"))
+Promise.resolve().then(() => console.log("promise 2"));
+
+
 console.log("end")
 
 process.on("exit", () => console.log("exit"))
